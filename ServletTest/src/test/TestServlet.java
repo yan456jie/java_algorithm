3b58e3187685780b57a3549c53ac35d9b83e0a0a
yan456jie <yan456jie@163.com>
2016-09-08 21:05:40 +0800

    firt eclipse commit

---
+++ ServletTest/src/test/TestServlet.java
@@ -0,0 +1,18 @@
+package test;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class TestServlet extends HttpServlet {
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		out.println("<html><body><h1>This is a servlet TestServlet.</h1></body></html>");
+		out.flush();
+	}
+}
